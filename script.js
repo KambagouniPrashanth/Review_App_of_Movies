0490cb319d1a500fa56e6b743a1f9d2077a88523
Lovegruru <kambagouniprashanth846@gmail.com>
2023-08-15 17:47:36 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,67 +1,110 @@
-let endpoint=`http://www.omdbapi.com/?i=tt3896198&apikey=d6f8fa89`
-let container=document.getElementById("container")
+const btn=document.getElementById("serach-btn")
+const movies_results=document.getElementById("movie-results")
+function displayMovies(){
+//     let spinner= document.getElementsByClassName("custom-spinning");
+//    spinner.style.display="block";
+    const apiKey=document.getElementById("apiKey").value;
+    const movieTitle=document.getElementById("movieTitle").value;
+    console.log(apiKey,movieTitle)
+    if(apiKey=="" || movieTitle==""){
+        showError("Both fieds are Required")
+        console.log("Both fieds are Required")
+        return;
+    }
+    let url=`https://www.omdbapi.com/?s=${movieTitle}&apikey=${apiKey}`;
+    document.getElementById("custom-spinning").style.display="block";
 
-async function imdbDetails(){
-    try{
-        let response=await fetch(endpoint,{method:"GET"})
+    async function movieDetails(){
+        try{
+
+        let response=await fetch(url,{method:"GET"});
         let result=await response.json();
-        console.log(result)
-        for(let i=0;i<20;i++){
-            addDataOnUI(result)
+        addDataOnUI(result.Search)
+        document.getElementById("custom-spinning").style.display="none";
+
+
+        }
+        catch(e){
+            showError(e)
         }
         
-        // console.log(result.Title)
-        // console.log(result.Year)
-        // console.log(result.Rated)
-        // console.log(result.Released)
-        // console.log(result.Ratings)
-        
+
+
     }
-    catch(e){
-        console.log(e)
-    }
-   
+    movieDetails();
 }
-imdbDetails()
 
+//     fetch(url)
+//     .then(response=>response.json())
+//     .then(data=>
+//         // console.log(data)
+//         addDataOnUI(data.Search);
+//         document.getElementById("custom-spinning").style.display="none";
+
+    
+
+         
+//         )
+
+// }
 function addDataOnUI(data){
-    let imdb_container=document.createElement("div")
-    imdb_container.className="imdb";
-    imdb_container.id="imdb"
+    console.log(data[0])
     
-        imdb_container.innerHTML=`
+    data.forEach((Element,index) => {
+        document.getElementById("serach-btn").style.display="none";
+        console.log("index")
+        const imdb=document.createElement("div")
+        imdb.className="imdb";
+        imdb.innerHTML=`
         <div class="poster">
-        <img src="${data.Poster}" alt="wednesday">
-    </div>
-    <div class="title">
-        ${data.Title}
-    </div>
-    <div class="year">
-    ${data.Released}
-    </div>
+        <img src="${Element.Poster}" alt="wednesday">
+        </div>
+          
+            <div class="index-title">
+                        
+            <p id="index">${index+1}<p>
+            
+            </div>
 
-    <div class="rating">
-
-    <span>${data.Ratings[0].Value}</span>
-    <span>${data.Ratings[0].Source}</span><br>
-    <span>${data.Ratings[1].Value}</span>
-    <span>${data.Ratings[1].Source}</span><br>
-    <span>${data.Ratings[2].Value}</span>
-    <span>${data.Ratings[2].Source}</span><br>
+        <div class="year-imdbid">
+        <p>${Element.Title}<p>
+        <p> ${Element.Year}<p>
+        <p>${Element.imdbID}</p>
+         
+        </div>
+        <div class="type">
+            <p>${Element.Type}</p>
+            
+        </div>
+    `;
+    movies_results.appendChild(imdb);
+        
+    });
     
-    
-    </div>
-    <div class="type">
-    ${data.Type}
-    
-    </div>`
-    container.appendChild(imdb_container)
 
 
+
+}
+
+function showError(message){
+  document.getElementById("error-handling").innerText=message;
     
   
-
 }
+// Poster
+// : 
+// "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+// Title
+// : 
+// "The Avengers"
+// Type
+// : 
+// "movie"
+// Year
+// : 
+// "2012"
+// imdbID
+// : 
+// "tt0848228"
 
-
-
+btn.addEventListener("click",displayMovies)
